1320377bf62ee3da34244af86e7246e223f2bf9b
herkulanus998 <herkulanusyulius@gmail.com>
2022-06-15 21:51:11 +0700

    my portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#FF0066",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
